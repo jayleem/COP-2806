7b1d1a9bb55fc658312df918c24b33ef0d74c0cc
Jason <Jason@DESKTOP-8MTKPST>
2019-05-05 16:36:11 -0400

    Merge origin/master
    Conflicts:
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	src/java/TOBA/ui/Account.java
    	src/java/TOBA/ui/LoginServlet.java
    	web/META-INF/context.xml
    	web/WEB-INF/web.xml
    	web/css/stylesheet.css
    	web/header.html

--- src/java/TOBA/ui/LoginServlet.java
+++ src/java/TOBA/ui/LoginServlet.java
@@ -5,11 +5,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import javax.servlet.http.HttpSession;
 
 /**
